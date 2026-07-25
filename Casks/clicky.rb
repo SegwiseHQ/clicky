f48d7f3fce7f750926f3b540858c0afe913f587c
shobhit-seg <shobhit@segwise.ai>
2026-07-25 15:40:22 +0530

    Bump version to 0.2.0

--- Casks/clicky.rb
+++ Casks/clicky.rb
@@ -1,5 +1,5 @@
 cask "clicky" do
-  version "0.1.3"
+  version "0.2.0"
   sha256 "REPLACE_WITH_ARM64_SHA256"
 
   url "https://github.com/SegwiseHQ/clicky/releases/download/v#{version}/clicky-macos-arm64.zip"
